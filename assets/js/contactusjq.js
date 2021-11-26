689ac2c594f13546a3d9ff3c72b04591651b7667
gemlibra <93558651+gemlibra@users.noreply.github.com>
2021-11-26 18:18:22 -0500

    Add files via upload
    These are files (html, css, js and image files) for my private website demo.

---
+++ assets/js/contactusjq.js
@@ -0,0 +1,23 @@
+
+
+ 
+ $(document).ready(function(){
+     // Open modal on page load
+     $("#myModal").modal('show');
+
+     // Close modal on button click
+     $(".btn").click(function(){
+         $("#myModal").modal('hide');
+     });
+
+     $(".container").fadeIn(15000);
+ }); 
+
+
+ //iFrame map fades-in 
+ $(document).ready(()=> {
+
+    $(".containerid").fadeIn(10000);
+
+ }) 
+
